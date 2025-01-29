3663b60d418a906a7f21c28cd29d87cc210a3d01
theodaguier <theo.daguier@icloud.com>
2025-01-29 11:33:01 +0100

    feat: 🎸 contact form design ok

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,10 @@
-import { PricingSection } from './_components/pricing-section'
-
+import { PricingSection } from '@/app/(web)/_components/pricing-section'
+import { ContactSection } from '@/app/(web)/_components/contact-section'
 export default function Page() {
   return (
     <div className="text-xl container h-screen">
       <PricingSection />
+      <ContactSection />
     </div>
   )
 }
--- src/components/layout/landing-section.tsx
+++ src/components/layout/landing-section.tsx
@@ -4,14 +4,24 @@ export const LandingSection = ({
   title,
   children,
   className,
+  subtitle,
+  description,
 }: {
   title: string
   children: React.ReactNode
   className?: string
+  subtitle?: string
+  description: () => React.ReactNode
 }) => {
   return (
-    <section className={cn('flex flex-col gap-8 mt-16', className)}>
-      <LandingTitle title={title} />
+    <section className={cn('flex flex-col gap-8 mt-16 md:mt-32', className)}>
+      <div className={cn('flex-1 flex flex-col gap-1', description != null && 'justify-between')}>
+        <div className="flex flex-col gap-2">
+          <LandingTitle title={title} />
+          {subtitle && <LandingSubtitle subtitle={subtitle} />}
+        </div>
+        {description != null && description()}
+      </div>
       {children}
     </section>
   )
@@ -19,4 +29,14 @@ export const LandingSection = ({
 
 export const LandingTitle = ({ title, className }: { title: string; className?: string }) => {
   return <h2 className={cn('', className)}>{title}</h2>
+}
+
+export const LandingSubtitle = ({
+  subtitle,
+  className,
+}: {
+  subtitle: string
+  className?: string
+}) => {
+  return <h2 className={cn('text-neutral-200', className)}>{subtitle}</h2>
 }
