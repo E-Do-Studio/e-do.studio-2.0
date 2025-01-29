384ec51aeeba58563a868492c339b06db109686e
“Ziyad <ziyadclombe.pro@gmail.com>
2025-01-29 16:27:23 +0100

    carousel our customers terminé

---
+++ src/app/(web)/_components/our-customers.tsx
@@ -0,0 +1,102 @@
+"use client"
+
+import { LandingSection } from "@/components/layout/landing-section"
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image"
+import Autoplay from "embla-carousel-autoplay"
+
+interface Customer {
+    img: string;
+    name: string;
+}
+
+export function OurCustomers() {
+    // IMG suctomers
+    const customers: Customer[] = [
+        {
+            img: 'https://www.citypng.com/public/uploads/preview/louis-vuitton-gold-logo-hd-png-701751694771300ifegnuqnmd.png',
+            name: 'Louis Vuitton'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/874/117/png-clipart-gucci-fashion-logo-others-miscellaneous-text.png',
+            name: 'Gucci'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/49/17/png-clipart-glasses-brand-logo-trademark-product-glasses-text-trademark.png',
+            name: 'Brand 3'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/874/117/png-clipart-gucci-fashion-logo-others-miscellaneous-text.png',
+            name: 'Brand 4'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/874/117/png-clipart-gucci-fashion-logo-others-miscellaneous-text.png',
+            name: 'Brand 5'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/874/117/png-clipart-gucci-fashion-logo-others-miscellaneous-text.png',
+            name: 'Brand 6'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/874/117/png-clipart-gucci-fashion-logo-others-miscellaneous-text.png',
+            name: 'Brand 7'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/874/117/png-clipart-gucci-fashion-logo-others-miscellaneous-text.png',
+            name: 'Brand 8'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/874/117/png-clipart-gucci-fashion-logo-others-miscellaneous-text.png',
+            name: 'Brand 9'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/874/117/png-clipart-gucci-fashion-logo-others-miscellaneous-text.png',
+            name: 'Brand 10'
+        },
+        {
+            img: 'https://e7.pngegg.com/pngimages/874/117/png-clipart-gucci-fashion-logo-others-miscellaneous-text.png',
+            name: 'Brand 11'
+        }
+    ]
+    return (
+        <LandingSection title="Nos clients les plus fidèles">
+            <div className="w-full mx-auto border-b border-t border-black">
+                <Carousel
+                    opts={{
+                        align: "start",
+                        loop: true,
+                        skipSnaps: false,
+                    }}
+                    plugins={[
+                        Autoplay({
+                            delay: 1500,
+                        }),
+                    ]}
+                    className="w-full"
+                >
+                    <CarouselContent className="-ml-1 flex items-center">
+                        {customers.map((customer, index) => (
+                            <CarouselItem key={index} className="pl-1 basis-1/4">
+                                <div className="p-2">
+                                    <Image
+                                        src={customer.img}
+                                        alt={customer.name}
+                                        width={100}
+                                        height={100}
+                                        className="w-full h-auto object-contain"
+                                    />
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                </Carousel>
+            </div>
+        </LandingSection>
+    )
+}
--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -2,13 +2,14 @@ import { PricingSection } from '@/app/(web)/_components/pricing-section'
 import { ContactSection } from '@/app/(web)/_components/contact-section'
 import { ServiceSection } from './_components/service-section'
 import { Location } from './_components/location'
-
+import { OurCustomers } from './_components/our-customers'
 export default function Page() {
 
   return (
     <div className="text-xl container min-h-screen">
       <ServiceSection />
       <PricingSection />
+      <OurCustomers />
       <ContactSection />
       <Location />
     </div>
