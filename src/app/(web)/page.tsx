ac01382f61020e1f13fdeb35da2343eb4ad2e721
“Ziyad <ziyadclombe.pro@gmail.com>
2025-01-30 16:52:08 +0100

    introduction en cours

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -3,15 +3,20 @@ import { ContactSection } from '@/app/(web)/_components/contact-section'
 import { ServiceSection } from './_components/service-section'
 import { Location } from './_components/location'
 import { OurCustomers } from './_components/our-customers'
+import { Introduction } from './_components/introduction'
+
 export default function Page() {
 
   return (
-    <div className="text-xl container min-h-screen">
-      <ServiceSection />
-      <PricingSection />
-      <OurCustomers />
-      <ContactSection />
-      <Location />
+    <div className="text-xl min-h-screen">
+      <Introduction />
+      <div className="container">
+        <ServiceSection />
+        <PricingSection />
+        <OurCustomers />
+        <ContactSection />
+        <Location />
+      </div>
     </div>
   )
 }
