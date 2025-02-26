ce47e91932b87e2926b3beeb60a314a648bbc0c2
“Ziyad <ziyadclombe.code@gmail.com>
2025-02-26 15:40:20 +0100

    modif post-prod

--- src/app/(web)/post-production/[category]/_components/description.tsx
+++ src/app/(web)/post-production/[category]/_components/description.tsx
@@ -1,15 +1,18 @@
 'use client'
 
 import { useTranslation } from 'react-i18next'
-
+import { Button } from '@/components/ui/button'
 export function Description() {
     const { t } = useTranslation('post-prod')
 
     return (
-        <div className="mb-8">
+        <div className="mb-8 flex flex-col gap-4">
             <p className="text-muted-foreground text-lg leading-relaxed">
                 {t('post-production.description')}
             </p>
+            <Button className='w-fit'>
+                {t('post-production.contact')}
+            </Button>
         </div>
     )
 } 
