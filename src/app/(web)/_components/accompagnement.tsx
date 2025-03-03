679030ac1af81766a9441e6697bc93a7230dfb06
“Ziyad <ziyadclombe.code@gmail.com>
2025-03-03 11:48:28 +0100

    modification texte et trad terminé

--- src/app/(web)/_components/accompagnement.tsx
+++ src/app/(web)/_components/accompagnement.tsx
@@ -7,8 +7,13 @@ import { useTranslation } from "react-i18next"
 export function Accompagnement() {
     const { t } = useTranslation("home")
 
+    const selfServiceDescription = t("accompagnement.self_service.description")
+    const fullServiceDescription = t("accompagnement.full_service.description")
+    const selfServiceDescriptionLines = selfServiceDescription.split("\n")
+    const fullServiceDescriptionLines = fullServiceDescription.split("\n")
+
     return (
-        <LandingSection title="Accompagnement">
+        <LandingSection title={t("accompagnement.title")}>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                     <motion.div
                         className="space-y-6 p-6 bg-neutral-100 rounded-lg"
@@ -21,7 +26,12 @@ export function Accompagnement() {
                     >
                         <h2 className="text-2xl font-bold"> {t("accompagnement.self_service.title")} </h2>
                         <p className="text-gray-600">
-                            {t("accompagnement.self_service.description")}
+                            {selfServiceDescriptionLines.map((line, index) => (
+                                <span key={index}>
+                                    {line}
+                                    {index < selfServiceDescriptionLines.length - 1 && <br />}
+                                </span>
+                            ))}
                         </p>
                     </motion.div>
 
@@ -36,7 +46,12 @@ export function Accompagnement() {
                     >
                         <h2 className="text-2xl font-bold"> {t("accompagnement.full_service.title")} </h2>
                         <p className="text-gray-600">
-                            {t("accompagnement.full_service.description")}
+                            {fullServiceDescriptionLines.map((line, index) => (
+                                <span key={index}>
+                                    {line}
+                                    {index < fullServiceDescriptionLines.length - 1 && <br />}
+                                </span>
+                            ))}
                         </p>
                     </motion.div>
                 </div>
